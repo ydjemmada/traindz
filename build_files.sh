b10ccb6eebe6d2ffb5ee35b5284fd04e95c79f6a
ydjemmada <yahia.djemmada@nhsm.edu.dz>
2025-11-26 14:30:17 +0100

    Fix: Use python3 in build script

--- build_files.sh
+++ build_files.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 echo "Building project..."
-python3.12 -m pip install -r requirements.txt
+python3 -m pip install -r requirements.txt
 
 echo "Collecting static files..."
-python3.12 backend/manage.py collectstatic --noinput --clear
+python3 backend/manage.py collectstatic --noinput --clear
 
 echo "Build complete!"
